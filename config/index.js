7c11c19adb8dfbbc6fe2843e146f007714a7dea8
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-04-14 13:15:25 -0500

    Preparando API para producción

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const db = {
+  dev: process.env.NODE_ENV !== 'production',
   dbUser: process.env.DB_USER,
   dbPasswd: process.env.DB_PASSWD,
   dbHost: process.env.DB_HOST,
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require('cors')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -10,17 +11,20 @@ const resolvers = require('./lib/resolvers')
 const { db } = require('./config/index')
 
 const app = express()
+const isDev = db.dev
 
 // Definiendo el schema
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
 const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+app.use(cors())
 
 app.use(
   '/api',
   gqlMiddleware({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true
+    graphiql: isDev
   })
 )
 
